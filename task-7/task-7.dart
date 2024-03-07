a28f9cfe6fa7cfac341deba1fc1c3d4ff0a4dec1
Polina Yudina <wispy@list.ru>
2024-03-07 22:46:00 +0300

    Задание — Магазин "Шестёрочка"

---
+++ task-7/task-7.dart
@@ -0,0 +1,181 @@
+void main() {
+  print(convertation(list));
+}
+
+final result = <String, Map<String, List<String>>>{};
+
+Map<String, Map<String, List<String>>> convertation(List<RawProductItem> data) {
+  for (final item in data) {
+    if (item.qty > 0 && item.expirationDate.isAfter(DateTime.now())) {
+      final name = item.name;
+      final category = item.categoryName;
+      final subcategory = item.subcategoryName;
+
+      if (!result.containsKey(category)) {
+        result[category] = {};
+      }
+      final categoryList = result[category];
+      if (categoryList != null) {
+        if (!categoryList.containsKey(subcategory)) {
+          categoryList[subcategory] = [];
+        }
+        final subcategoryList = categoryList[subcategory];
+        if (subcategoryList != null) {
+          subcategoryList.add(name);
+        }
+      }
+    }
+  }
+  return result;
+}
+
+class RawProductItem {
+  final String name;
+  final String categoryName;
+  final String subcategoryName;
+  final DateTime expirationDate;
+  final int qty;
+
+  const RawProductItem(
+      {required this.name,
+      required this.categoryName,
+      required this.subcategoryName,
+      required this.expirationDate,
+      required this.qty});
+}
+
+final list = [
+  RawProductItem(
+    name: 'Персик',
+    categoryName: 'Растительная пища',
+    subcategoryName: 'Фрукты',
+    expirationDate: DateTime(2022, 12, 22),
+    qty: 5,
+  ),
+  RawProductItem(
+    name: 'Молоко',
+    categoryName: 'Молочные продукты',
+    subcategoryName: 'Напитки',
+    expirationDate: DateTime(2022, 12, 22),
+    qty: 5,
+  ),
+  RawProductItem(
+    name: 'Кефир',
+    categoryName: 'Молочные продукты',
+    subcategoryName: 'Напитки',
+    expirationDate: DateTime(2022, 12, 22),
+    qty: 5,
+  ),
+  RawProductItem(
+    name: 'Творог',
+    categoryName: 'Молочные продукты',
+    subcategoryName: 'Не напитки',
+    expirationDate: DateTime(2022, 12, 22),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Творожок',
+    categoryName: 'Молочные продукты',
+    subcategoryName: 'Не напитки',
+    expirationDate: DateTime(2023, 12, 22),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Творог',
+    categoryName: 'Молочные продукты',
+    subcategoryName: 'Не напитки',
+    expirationDate: DateTime(2024, 12, 22),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Гауда',
+    categoryName: 'Молочные продукты',
+    subcategoryName: 'Сыры',
+    expirationDate: DateTime(2023, 12, 22),
+    qty: 3,
+  ),
+  RawProductItem(
+    name: 'Маасдам',
+    categoryName: 'Молочные продукты',
+    subcategoryName: 'Сыры',
+    expirationDate: DateTime(2024, 12, 22),
+    qty: 2,
+  ),
+  RawProductItem(
+    name: 'Яблоко',
+    categoryName: 'Растительная пища',
+    subcategoryName: 'Фрукты',
+    expirationDate: DateTime(2024, 12, 4),
+    qty: 4,
+  ),
+  RawProductItem(
+    name: 'Морковь',
+    categoryName: 'Растительная пища',
+    subcategoryName: 'Овощи',
+    expirationDate: DateTime(2024, 12, 23),
+    qty: 51,
+  ),
+  RawProductItem(
+    name: 'Черника',
+    categoryName: 'Растительная пища',
+    subcategoryName: 'Ягоды',
+    expirationDate: DateTime(2024, 12, 25),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Курица',
+    categoryName: 'Мясо',
+    subcategoryName: 'Птица',
+    expirationDate: DateTime(2024, 12, 18),
+    qty: 2,
+  ),
+  RawProductItem(
+    name: 'Говядина',
+    categoryName: 'Мясо',
+    subcategoryName: 'Не птица',
+    expirationDate: DateTime(2023, 12, 17),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Телятина',
+    categoryName: 'Мясо',
+    subcategoryName: 'Не птица',
+    expirationDate: DateTime(2023, 12, 17),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Индюшатина',
+    categoryName: 'Мясо',
+    subcategoryName: 'Птица',
+    expirationDate: DateTime(2024, 12, 17),
+    qty: 1,
+  ),
+  RawProductItem(
+    name: 'Утка',
+    categoryName: 'Мясо',
+    subcategoryName: 'Птица',
+    expirationDate: DateTime(2024, 12, 18),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Гречка',
+    categoryName: 'Растительная пища',
+    subcategoryName: 'Крупы',
+    expirationDate: DateTime(2024, 12, 22),
+    qty: 8,
+  ),
+  RawProductItem(
+    name: 'Свинина',
+    categoryName: 'Мясо',
+    subcategoryName: 'Не птица',
+    expirationDate: DateTime(2024, 12, 23),
+    qty: 5,
+  ),
+  RawProductItem(
+    name: 'Груша',
+    categoryName: 'Растительная пища',
+    subcategoryName: 'Фрукты',
+    expirationDate: DateTime(2024, 12, 25),
+    qty: 5,
+  ),
+];
