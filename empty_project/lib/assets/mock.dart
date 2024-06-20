a9b4f65dfca3c693c8660a45b4b32f90915332eb
Polina Yudina <wispy@list.ru>
2024-06-20 15:21:04 +0300

    photo_galery

---
+++ empty_project/lib/assets/mock.dart
@@ -0,0 +1,22 @@
+import 'package:change_theme/assets/app_strings.dart';
+
+const profilePhotos = [
+  ImgStrings.profileImg1,
+  ImgStrings.profileImg2,
+  ImgStrings.profileImg3,
+  ImgStrings.profileImg4,
+  ImgStrings.profileImg5,
+  ImgStrings.profileImg6,
+  ImgStrings.profileImg7,
+  ImgStrings.profileImg8,
+  ImgStrings.profileImg9,
+  ImgStrings.profileImg10,
+  ImgStrings.profileImg11,
+  ImgStrings.profileImg12,
+  ImgStrings.profileImg13,
+  ImgStrings.profileImg14,
+  ImgStrings.profileImg15,
+  ImgStrings.profileImg16,
+  ImgStrings.profileImg17,
+  ImgStrings.profileImg18,
+];
